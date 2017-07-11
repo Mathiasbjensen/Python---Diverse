b8b9039b01cf319878c3367ae26a8ca8a1724c4d
Mathias Jensen <mathiasbj94@gmail.com>
2017-07-11 22:38:26 +0200

    First commit

---
+++ ClassTest.py
@@ -0,0 +1,22 @@
+class Enemy:
+    life = 3
+
+    def attack(self):
+        print("ouch!")
+        self.life -= 1
+
+    def checkLife(self):
+        if self.life <= 0:
+            print("I am dead")
+        else:
+            print(str(self.life) + " life left")
+
+
+# Making an object.
+# Objects are a way to access the stuff inside a class
+# An object is a copy of the class (instans?)
+
+enemy1 = Enemy()
+
+enemy1.attack()
+enemy1.checkLife()
