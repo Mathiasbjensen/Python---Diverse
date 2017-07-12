510c5f0891854f1f5a560fc8d5447c3fa9d750c4
Mathias Jensen <mathiasbj94@gmail.com>
2017-07-12 14:41:30 +0200

    Added try - except

--- APCalculator.py
+++ APCalculator.py
@@ -11,19 +11,30 @@ def calculate(rating):
             points = 1511.26/(1+1639.28*math.exp(-0.00412*rating))
     points = int(points)
 
+    # Clearing the entries so we continue to only have 1 number in each entry.
     entry_Points2.delete(0, "end")
     entry_Points3.delete(0, "end")
     entry_Points5.delete(0, "end")
+
+    # Inserts the points into the entrie given the rating
     entry_Points2.insert(0, int(points*0.76))
     entry_Points3.insert(0, int(points * 0.88))
     entry_Points5.insert(0, points)
 
 
+# Dynamically updating the points when rating is changed
 def callback(sv):
-    calculate(int(sv.get()))
+    try:
+        calculate(int(sv.get()))
+    except ValueError:
+        print("Only numbers you imbecile")
+
+
+
 
 root = Tk()
 sv = StringVar()
+# Calls the callback function which updates the points.
 sv.trace("w", lambda name, index, mode, sv=sv: callback(sv))
 
 label_Rating = Label(root, text="Rating")
@@ -36,17 +47,17 @@ entry_Points2 = Entry(root)
 entry_Points3 = Entry(root)
 entry_Points5 = Entry(root)
 
-
+# Labels
 label_Rating.grid(row=1)
 entry_Rating.grid(row=1, column=1)
 label_twos.grid(row=0, column=2, padx=10, sticky=E)
 label_threes.grid(row=1, column=2, padx=10, sticky=E)
 label_fives.grid(row=2, column=2, padx=10, sticky=E)
 
+# Entries
 entry_Points2.grid(row=0, column=3)
 entry_Points3.grid(row=1, column=3)
 entry_Points5.grid(row=2, column=3)
-
 
 root.mainloop()
 
